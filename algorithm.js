14cd312621d07616e287e578f19b328956afc938
Kevin Washington <kevw12188@gmail.com>
2015-09-14 11:25:23 -0400

    problem solved

--- algorithm.js
+++ algorithm.js
@@ -2,6 +2,32 @@ function checkIfTestsAreWorking() {
     return "Tests are working";
 }
 
+function sumOfTheSquaresOfRange(rangeStart, rangeStop) {
+    var sumOfSquaresOfRange = 0;
+    while(!(rangeStart > rangeStop)) {
+        sumOfSquaresOfRange += rangeStart * rangeStart;
+        rangeStart++;
+    }
+    return sumOfSquaresOfRange;
+}
 
+function squareOfTheSumOfRange(rangeStart, rangeStop) {
+    var sumOfRange = 0;
+    while(!(rangeStart > rangeStop)) {
+        sumOfRange += rangeStart;
+        rangeStart++;
+    }
+    return sumOfRange * sumOfRange;
+}
 
+function findDifference(value1, value2) {
+    var largerNumber = (value1 > value2) ? value1 : value2;
+    var smallerNumber = (value1 < value2) ? value1 : value2;
+    return largerNumber - smallerNumber;
+}
 
+function findDifferenceOfSumOfRangeAndSquareOfSumOfRange(rangeStart, rangeStop) {
+    var sumOfSquaresOfRange = sumOfTheSquaresOfRange(rangeStart, rangeStop);
+    var squareOfSumOfRange = squareOfTheSumOfRange(rangeStart, rangeStop);
+    return findDifference(sumOfSquaresOfRange, squareOfSumOfRange);
+}
--- algorithm.test.js
+++ algorithm.test.js
@@ -1,6 +1,41 @@
 describe("Tests are working", function() {
     it("should return Tests are working", function() {
-        result = checkIfTestsAreWorking();
+        var result = checkIfTestsAreWorking();
         expect(result).toBe("Tests are working");
+    });
+});
+
+describe("sumOfTheSquaresOfRange", function() {
+    it("should return 385 when arguments are int 1, and int 10", function() {
+        var result = sumOfTheSquaresOfRange(1, 10);
+        expect(result).toBe(385);
+    });
+});
+
+describe("squareOfTheSumOfRange", function() {
+    it("should return 3025, when arguments are int 1, and int 10", function() {
+        var result = squareOfTheSumOfRange(1,10);
+        expect(result).toBe(3025);
+    });
+});
+
+describe("findDifference", function() {
+    it("should return 2640, when arguments are int 385, and int 3025", function() {
+        var result = findDifference(385, 3025);
+        expect(result).toBe(2640);
+    });
+});
+
+describe("findDifferenceOfSumOfRangeAndSquareOfSumOfRange", function() {
+    it("should return 2640, when arguments are int 1, and int 10", function () {
+        var result = findDifferenceOfSumOfRangeAndSquareOfSumOfRange(1, 10);
+        expect(result).toBe(2640);
+    });
+});
+
+describe("problem solved", function() {
+    it("should return answer to problem", function() {
+        var result = findDifferenceOfSumOfRangeAndSquareOfSumOfRange(1, 100);
+        expect(result).toBe(25164150);
     });
 });
